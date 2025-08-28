00d1817438b582a02be93fe656f020de041d51bc
ryuchaerin <dmsquf1009@dgu.ac.kr>
2025-08-28 12:04:40 +0900

    :recycle: refactor: 게임 리스트/대기방 웹소켓 리팩토링

--- src/main/java/com/wepong/pongdang/controller/GameRoomController.java
+++ src/main/java/com/wepong/pongdang/controller/GameRoomController.java
@@ -1,29 +1,67 @@
 package com.wepong.pongdang.controller;
 
 import com.wepong.pongdang.dto.response.ChatResponseDTO;
+import com.wepong.pongdang.dto.response.TurtlePlayerDTO;
+import com.wepong.pongdang.model.multi.turtle.PlayerService;
 import com.wepong.pongdang.service.AuthService;
+import com.wepong.pongdang.service.GameRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
-import java.security.Principal;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class GameRoomController {
 
-    private final SimpMessagingTemplate messagingTemplate;
     private final AuthService authService;
+    private final PlayerService playerService;
+    private final GameRoomService gameRoomService;
 
     // 채팅
-    @MessageMapping("/{roomId}/chat")
-    public void handleGameAction(@DestinationVariable Long roomId, ChatResponseDTO chat, Principal principal) {
-        Long userId = Long.valueOf(principal.getName());
-        String nickname = authService.findById(userId).getNickname();
-        chat.setSender(nickname);
+    @MessageMapping("/gameroom/chat/{roomId}")
+    public void handleGameAction(@DestinationVariable Long roomId, String msg, StompHeaderAccessor accessor) {
+        String nickname = (String) accessor.getSessionAttributes().get("nickname");
 
-        messagingTemplate.convertAndSend("/topic/chat/" + roomId, chat);
+        ChatResponseDTO chat = ChatResponseDTO.builder()
+                .message(msg)
+                .sender(nickname)
+                .build();
+
+        gameRoomService.sendRoom(roomId, "chat", chat);
+    }
+
+    // 거북이 선택
+    @MessageMapping("/gameroom/choice/{roomId}")
+    public void handleChoice(@DestinationVariable Long roomId, Map<String, String> payload, SimpMessageHeaderAccessor accessor) {
+        Long userId = (Long) accessor.getSessionAttributes().get("userId");
+        TurtlePlayerDTO player = playerService.getPlayer(roomId, userId);
+        player.setTurtleId(payload.get("turtle_id"));
+
+        List<TurtlePlayerDTO> players = playerService.getPlayers(roomId);
+        gameRoomService.sendRoom(roomId, "choice", players);
+    }
+
+    // 준비 완료
+    @MessageMapping("/gameroom/ready/{roomId}")
+    public void handleReady(@DestinationVariable Long roomId, Map<String, Boolean> payload, SimpMessageHeaderAccessor accessor) {
+        Long userId = (Long) accessor.getSessionAttributes().get("userId");
+        TurtlePlayerDTO player = playerService.getPlayer(roomId, userId);
+        player.setReady(payload.get("isReady"));
+
+        List<TurtlePlayerDTO> players = playerService.getPlayers(roomId);
+        gameRoomService.sendRoom(roomId, "ready", players);
+    }
+
+    // 게임 시작
+    @MessageMapping("/gameroom/start/{roomId}")
+    public void handleStart(@DestinationVariable Long roomId) {
+        gameRoomService.sendRoom(roomId, "start", "/multi/" + roomId + "/turtlerun");
+        gameRoomService.sendList("list", gameRoomService.selectAll());
     }
 }
