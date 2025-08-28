8a10cdfc3b64938a890413445d3d411c25887620
ryuchaerin <dmsquf1009@dgu.ac.kr>
2025-08-28 11:58:02 +0900

    :memo: edit: 헤더 토큰 추출 후 웹소켓 세션에 userId 저장

--- src/main/java/com/wepong/pongdang/socket/StompAuthChannelInterceptor.java
+++ src/main/java/com/wepong/pongdang/socket/StompAuthChannelInterceptor.java
@@ -3,13 +3,11 @@ package com.wepong.pongdang.socket;
 import com.wepong.pongdang.exception.InvalidTokenException;
 import com.wepong.pongdang.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -28,14 +26,30 @@ public class StompAuthChannelInterceptor implements ChannelInterceptor {
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             // 헤더에서 토큰 추출 후 검증
             String authHeader = accessor.getFirstNativeHeader("Authorization");
-            if(authHeader == null) throw new InvalidTokenException();
+
+            if (authHeader == null || authHeader.isEmpty()) {
+                throw new InvalidTokenException();
+            }
 
             Long userId = authService.validateAndGetUserId(authHeader);
+            accessor.getSessionAttributes().put("userId", userId);
+        } else if(StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
+            String destination = accessor.getDestination();
 
-            // Principal 구현
-            UsernamePasswordAuthenticationToken auth =
-                    new UsernamePasswordAuthenticationToken(userId, null, List.of());
-            accessor.setUser(auth);
+            // 구독 경로 추출
+            if(destination != null) {
+                if(destination.startsWith("/topic/gameroom")) {
+                    Long roomId = Long.parseLong(destination.substring("/topic/gameroom".length()));
+                    accessor.getSessionAttributes().put("roomId", roomId);
+                    accessor.getSessionAttributes().put("type", "room");
+                }
+                else if(destination.startsWith("/topic/game")) {
+                    accessor.getSessionAttributes().put("type", "game");
+                }
+                else if(destination.equals("/topic/gameroom")) {
+                    accessor.getSessionAttributes().put("type", "list");
+                }
+            }
         }
 
         // 메시지 그대로 컨트롤러에 반환
